baefe9c5cfca460084b8388c72f8d02825767c15
kuenzang24.dev <kuenzang24.dev@gmail.com>
2024-04-23 11:59:15 +0600

    add the form in shipping tab

---
+++ Screens/Cart/checkout/Confirm.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Confirm = () => {
+    return <Text> Confirm Works</Text>;
+};
+
+export default Confirm;
---
+++ Screens/Cart/checkout/Payment.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Payment = () => {
+    return <Text> Payment Works</Text>;
+};
+
+export default Payment;
