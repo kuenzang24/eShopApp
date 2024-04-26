bdac052089b2ed75b1f76529924cc9831132e1c4
kuenzang24.dev <kuenzang24.dev@gmail.com>
2024-04-26 13:59:44 +0600

    done with practical 12

--- Screens/Cart/checkout/Confirm.js
+++ Screens/Cart/checkout/Confirm.js
@@ -1,8 +1,93 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, StyleSheet, Dimensions,Button } from "react-native";
+import { Text, Left, Right, ListItem, Thumbnail, Body } from "native-base";
+import { connect } from "react-redux";
+import * as actions from "../../../Redux/Actions/cartActions";
+import { ScrollView } from "react-native-gesture-handler";
 
-const Confirm = () => {
-    return <Text> Confirm Works</Text>;
+var { height } = Dimensions.get("window");
+const Confirm = (props) => {
+    const confirm = props.route.params;
+
+    const confirmOrder = () => {
+        setTimeout(() => {
+            props.clearCart();
+            props.navigation.navigate("Cart");
+        });
+    };
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Confirm Order</Text>
+        {props.route.params ? (
+          <View style={{ borderWidth: 1, borderColor: "orange" }}>
+            <Text style={styles.shipping}>Shipping to: </Text>
+            <View style={{ padding: 8 }}>
+              <Text>
+                Address:
+                {confirm.order.order.shippingAddress1}
+              </Text>
+              <Text>
+                Address2:
+                {confirm.order.order.shippingAddress2}
+              </Text>
+              <Text>City: {confirm.order.order.city}</Text>
+              <Text>Zip Code: {confirm.order.order.zip}</Text>
+              <Text>Country: {confirm.order.order.country}</Text>
+            </View>
+            <Text style={styles.shipping}>Items:</Text>
+            {confirm.order.order.orderItems.map((x) => {
+              return (
+                <ListItem style={styles.listItem} key={x.product.name} avatar>
+                  <Left>
+                    <Thumbnail source={{ uri: x.product.image }} />
+                  </Left>
+                  <Body style={styles.body}>
+                    <Left>
+                      <Text>{x.product.name}</Text>
+                    </Left>
+                    <Right>
+                      <Text>${x.product.price}</Text>
+                    </Right>
+                  </Body>
+                </ListItem>
+              );
+            })}
+          </View>
+        ) : null}
+        <View style={{ alignItems: "center", margin: 20 }}>
+            <Button title={"Place Order"} onPress={confirmOrder} />
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
-export default Confirm;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        clearCart: () => dispatch(actions.clearCart()),
+    };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: height,
+    padding: 8,
+    alignContent: "center",
+    backgroundColor: "white",
+  },
+  titleContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 8,
+  },
+  shipping: {
+    alignSelf: "center",
+    margin: 8,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default connect(null, mapDispatchToProps)(Confirm);
